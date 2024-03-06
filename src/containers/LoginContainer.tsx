49bddda381a09304bfaee152207c417d0d04564d
Amy Yang <amy.yang7991@gmail.com>
2024-03-06 15:48:48 -0500

    apply onFocus and onBlur to the username and password input elements

--- src/containers/LoginContainer.tsx
+++ src/containers/LoginContainer.tsx
@@ -1,9 +1,16 @@
 import React, { MouseEventHandler, ReactElement } from "react";
 
 const LoginContainer = (): React.JSX.Element => {
-    const inputTransparent: MouseEventHandler = () => {
-        const input = document.getElementsByTagName('input');
-        input[0].placeholder = '';
+    const usernameFocusBlur = (event: any) => {
+        const username = document.getElementsByTagName('input')[0];
+        if (event.type === 'focus') username.placeholder = '';
+        else if (event.type === 'blur') username.placeholder = 'Username';
+    }
+
+    const passwordFocusBlur = (event: any) => {
+        const password = document.getElementsByTagName('input')[1];
+        if (event.type === 'focus') password.placeholder = '';
+        else if (event.type === 'blur') password.placeholder = 'Password';
     }
 
     return (
@@ -11,11 +18,11 @@ const LoginContainer = (): React.JSX.Element => {
             <section className="flex flex-col rounded-xl items-center border-solid border-black border-2 h-2/3 w-1/3">
                 <h1 className="flex justify-center mt-20 mb-36 items-center text-5xl h-10 w-60"> Taste Test </h1>
                 <form className="flex justify-center flex-col h-60">
-                    <input className="loginInput" type="text" placeholder="Username" id="username" onClick={inputTransparent}/>
-                    <input className="loginInput" type="text" placeholder="Password" id="password" />
+                    <input className="loginInput" type="text" placeholder="Username" id="username" onFocus={usernameFocusBlur} onBlur={usernameFocusBlur}/>
+                    <input className="loginInput" type="text" placeholder="Password" id="password" onFocus={passwordFocusBlur} onBlur={passwordFocusBlur}/>
                     <section className="flex justify-center">
-                        <button className="loginButtons" type="submit">Login</button>
-                        <button className="loginButtons" type="submit">Sign Up</button>
+                        <button className="loginButtons" type="button">Login</button>
+                        <button className="loginButtons" type="button">Sign Up</button>
                     </section>
                 </form>
             </section>
