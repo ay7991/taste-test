4b02229cfd9d405d95245063af4e30403044f252
Amy Yang <amy.yang7991@gmail.com>
2024-03-06 17:38:53 -0500

    add signup to routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import '../static/styles.scss';
 
 import LoginContainer from "../containers/LoginContainer";
+import SignupContainer from "../containers/SignupContainer";
 
 const App = (): React.JSX.Element => {
   return (
     <Router>
       <Routes>
           <Route path="/" element={<LoginContainer />}></Route>
+          <Route path="/signup" element={<SignupContainer />}></Route>
       </Routes>
     </Router>
   );
