fe8e05e72dd12dc05b96fab3941eb5ac62f75e7b
Amy Yang <amy.yang7991@gmail.com>
2024-03-08 02:28:22 -0500

    include home route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import '../static/styles.scss';
 
 import LoginContainer from "../containers/LoginContainer";
 import SignupContainer from "../containers/SignupContainer";
+import HomeContainer from "../containers/HomeContainer";
 
 const App = (): React.JSX.Element => {
   return (
@@ -12,6 +12,7 @@ const App = (): React.JSX.Element => {
       <Routes>
           <Route path="/" element={<LoginContainer />}></Route>
           <Route path="/signup" element={<SignupContainer />}></Route>
+          <Route path="/home" element={<HomeContainer />}></Route>
       </Routes>
     </Router>
   );
