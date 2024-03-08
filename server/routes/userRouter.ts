4394f11afe17440dafdf914db140f1047dc3d6c5
Amy Yang <amy.yang7991@gmail.com>
2024-03-08 02:27:09 -0500

    write GET request for login

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -3,8 +3,9 @@ const userController = require('../controllers/userController');
 const router = userExpress.Router();
 
 router.post('/signup', userController.postUser, (req: any, res: any) => {
-   console.log('in user router')
    return res.status(200); 
 });
+
+// router.get('/login')
 
 module.exports = router;
