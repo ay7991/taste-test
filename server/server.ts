da7d85eb2d91393bce754ed239b24fd01c471e81
Amy Yang <amy.yang7991@gmail.com>
2024-03-09 15:53:19 -0500

    include 404 and global error handling

--- server/server.ts
+++ server/server.ts
@@ -19,6 +19,23 @@ app.use('/api/user', userRouter);
 
 app.use(express.static(path.resolve(__dirname, "../build")));
 
+// if user makes a request that is not possible
+app.all('*', function(req: any, res: any) {
+  res.status(404).send('Status: 404');
+});
+
+// Global Error Handling
+app.use((err: any, req: any, res: any, next: any) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 });
